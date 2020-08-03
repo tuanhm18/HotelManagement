8f2916b1929b109937c11246a3db3fef7ee6cfd2
thanhskills <thanhnh18.cdsg@saigontech.edu.vn>
2020-08-03 21:39:23 +0700

    need help

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/admin/rooms/{id?}', 'Api\RoomController@get');
+
 Route::get('admin/services/{id?}', 'Api\ServiceController@get');
+
+Route::get('/admin/rooms/{id?}', 'Api\RoomController@get');
 Route::post('admin/rooms', 'Api\RoomController@create');
 Route::put('admin/rooms', 'Api\RoomController@update');
 Route::delete('admin/rooms/{id}','Api\RoomController@delete');
+
+Route::get('/admin/positions/{id?}', 'Api\Positonstroller@get');
+Route::post('admin/positions', 'Api\Positonstroller@create');
+Route::put('admin/positions', 'Api\Positonstroller@update');
+Route::delete('admin/positions/{id}','Api\Positonstroller@delete');
+
+Route::get('/admin/customers/{id?}', 'Api\Customerstroller@get');
+Route::post('admin/customers', 'Api\Customerstroller@create');
+Route::put('admin/customers', 'Api\Customerstroller@update');
+Route::delete('admin/customers/{id}','Api\Customerstroller@delete');
 // Route::group(['middleware'=>'auth'], function() {
 //     Route::get('/admin/rooms', 'Api/RoomController@get');
 // });
